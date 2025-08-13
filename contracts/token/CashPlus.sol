9ddb6b0b6ecb7753bedcc8ec1a0d1ed82488ca68
zhangwenhai <zhangwenhai6@163.com>
2025-08-13 20:48:36 +0800

    update code

--- contracts/token/CashPlus.sol
+++ contracts/token/CashPlus.sol
@@ -11,11 +11,11 @@ import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Ini
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {BaseStorage} from "../base/BaseStorage.sol";
-import "../Interfaces/ISAmMMF.sol";
+import "../Interfaces/ICashPlus.sol";
 import {Blacklistable} from "../BlackList/Blacklistable.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
-contract SAmMMF is
+contract CashPlus is
     Initializable,
     ERC20Upgradeable,
     ERC20PausableUpgradeable,
